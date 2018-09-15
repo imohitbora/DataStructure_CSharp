7a4220f37da75f545e4161e81dd245312238dd09
Mohit Bora <https://>
2018-09-15 20:04:24 +0530

    Testing 2

--- LinkList/DataStructure/Program.cs
+++ LinkList/DataStructure/Program.cs
@@ -1,9 +1,5 @@
 ﻿using DataStructure.LinkedList;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructure
 {
@@ -11,7 +7,6 @@ namespace DataStructure
     {
         public static void Main(string[] args)
         {
-
             PerformOperationOnSingleLinkList();
             Console.ReadKey();
         }
